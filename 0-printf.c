00fa0b25858a5d3b691e13f20c7b0409d1383dc1
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-08-08 23:52:37 +0000

    update on print f function task 0 done

--- 0-printf.c
+++ 0-printf.c
@@ -1,10 +1,46 @@
 #include "main.h"
-#include <strarg.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...)
 {
 	va_list args;
+	int p, n_printed =0, count;
+	char *str;
 	va_start(args, format);
+	p = 0;
 
-	va_end(arg);
+	while (format[p] != '\0')
+	{
+		if (format[p] != '%')
+		{
+			n_printed++;
+			_putchar(format[p]);
+		}
+		else
+		{
+			p++;
+			if (format[p] == 'c')
+			{
+				n_printed++;
+				_putchar(va_arg(args, int));
+			}
+			else if (format[p] == 's')
+			{
+				str = va_arg(args, char *);
+				for (count = 0; str[count] != '\0'; count++)
+				{
+					n_printed++;
+					_putchar(str[count]);
+				}
+			}
+			else if (format[p] == '%')
+			{
+				n_printed++;
+				_putchar(format[p]);
+			}
+		}
+		p++;
+	}
+	va_end(args);
+	return(n_printed);
 }
